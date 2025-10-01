4f4bc7b2e5da410f4831cfacb8c30473548fa2c0
michael.travnicek <michael.travnicek@sanezoo.com>
2025-10-02 01:50:07 +0200

    fix script

--- djangotutorial/superuser.py
+++ djangotutorial/superuser.py
@@ -1,7 +1,8 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangotutorial.settings")
+
 django.setup()
 
 from django.contrib.auth import get_user_model
